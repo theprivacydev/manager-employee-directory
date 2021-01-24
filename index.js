7addc208818fc933bf9c16ff31fa1e8469641141
Pro-Code-art <jayme.coder@gmail.com>
2021-01-24 14:00:04 -0800

    edited questions in index.js

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'Hello! Please enter the Team Manager\'s name to begin!',
+        name: 'managerName',
     },
     {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What is the manager\'s employee id?',
+        name: 'managerId',
     },
     {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What is the manager\'s email address?',
+        name: 'managerEmail',
     },
     {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What is the managers office number?',
+        name: 'officeNumber',
+    },
+    {
+        type: 'list',
+        message: 'Which employee would you like to add to finish building your team?',
+        name: 'addEmployee',
+        choices: [
+            'Engineer',
+            'Intern',
+        ],
     },
 ]
 
