8f60d9b41813ee16b78f6ccfa7c31ede0b4d160c
Pro-Code-art <jayme.coder@gmail.com>
2021-01-25 19:42:03 -0800

    added exports to js files and required them in generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+const Employee = require('../lib/employee.js');
+const Engineer = require('../lib/engineer.js');
+const Intern = require('../lib/intern.js');
+const Manager = require('../lib/manager.js');
+
+
 
 
 
